4b8abc1409a1572dead4ca6c80bb4a498aa524f1
Vicente Vaccaro <thunderstripe211@gmail.com>
2026-09-11 12:58:18 -0700

    Test for practicing OpModes - Vicente Vaccaro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear OpMode")
+@Disabled
+public class TestTeleop extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        Servo ballThing = hardwareMap.get(Servo.class, "ballThing");
+
+
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+        }
+    }
+}
