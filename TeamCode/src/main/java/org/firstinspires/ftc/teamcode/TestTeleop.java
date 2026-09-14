51a00f19dcbaca4a16016a09331fb96a8fdcc8be
DarianJones07 <156040663+DarianJones07@users.noreply.github.com>
2026-09-14 17:50:44 -0400

    added input/output

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -4,13 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear OpMode")
-@Disabled
 public class TestTeleop extends LinearOpMode {
     @Override
     public void runOpMode() {
@@ -18,8 +18,14 @@ public class TestTeleop extends LinearOpMode {
         DcMotor frontLeft = hardwareMap.get(DcMotor.class, "leftFront");
         DcMotor backRight = hardwareMap.get(DcMotor.class, "rightBack");
         DcMotor frontRight = hardwareMap.get(DcMotor.class, "rightFront");
-        Servo ballThing = hardwareMap.get(Servo.class, "ballThing");
 
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        DcMotor intakeFront = hardwareMap.get(DcMotor.class, "intakeFront");
+        DcMotor intakeBack = hardwareMap.get(DcMotor.class, "intakeBack");
+
+        intakeBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
         waitForStart();
@@ -41,6 +47,22 @@ public class TestTeleop extends LinearOpMode {
             backRight.setPower(backRightPower);
             frontLeft.setPower(frontLeftPower);
             frontRight.setPower(frontRightPower);
+
+            if (gamepad1.a)
+            {
+                intakeFront.setPower(0.5);
+                intakeBack.setPower(0.5);
+            }
+            else if (gamepad1.b)
+            {
+                intakeFront.setPower(-0.5);
+                intakeBack.setPower(-0.5);
+            }
+            else
+            {
+                intakeFront.setPower(0);
+                intakeBack.setPower(0);
+            }
 
         }
     }
